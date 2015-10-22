56ef10ab8ac08312438fc094f7bd7d0904f1d77a
Lucas Andrey <andreybleme1@gmail.com>
2015-10-22 00:42:00 -0200

    New maven project

---
+++ src/main/java/br/com/jxls_me/jxls_me/App.java
@@ -0,0 +1,13 @@
+package br.com.jxls_me.jxls_me;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
