81067cb8ee5f06edb83e87ef63a258cc6fdd0863
Lucas Andrey <andreybleme1@gmail.com>
2015-10-22 01:03:36 -0200

    New Jedi POJO created and JXLS dependency added

---
+++ src/main/java/br/com/jxls_me/jxls_me/Jedi.java
@@ -0,0 +1,68 @@
+/**
+ * A simple Jedi POJO to be exported.
+ */
+package br.com.jxls_me.jxls_me;
+
+import java.math.BigDecimal;
+
+/**
+ * @author Lucas
+ *
+ */
+public class Jedi {
+	
+	private String name;
+	private java.util.Date birthDate;
+    private BigDecimal payment;
+    private String lightSaberColor;
+    
+    /**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the birthDate
+	 */
+	public java.util.Date getBirthDate() {
+		return birthDate;
+	}
+	/**
+	 * @param birthDate the birthDate to set
+	 */
+	public void setBirthDate(java.util.Date birthDate) {
+		this.birthDate = birthDate;
+	}
+	/**
+	 * @return the payment
+	 */
+	public BigDecimal getPayment() {
+		return payment;
+	}
+	/**
+	 * @param payment the payment to set
+	 */
+	public void setPayment(BigDecimal payment) {
+		this.payment = payment;
+	}
+	/**
+	 * @return the lightSaberColor
+	 */
+	public String getLightSaberColor() {
+		return lightSaberColor;
+	}
+	/**
+	 * @param lightSaberColor the lightSaberColor to set
+	 */
+	public void setLightSaberColor(String lightSaberColor) {
+		this.lightSaberColor = lightSaberColor;
+	}
+
+}
